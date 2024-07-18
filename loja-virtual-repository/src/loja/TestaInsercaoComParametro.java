aa0f8f20c60dfdcf7499be40c244b4477b318e54
sbm-balmeida <beatriz.almeida@sbmtechnology.com.br>
2024-07-18 13:59:10 -0300

    Usando o try-with-resources

--- loja-virtual-repository/src/loja/TestaInsercaoComParametro.java
+++ loja-virtual-repository/src/loja/TestaInsercaoComParametro.java
@@ -11,32 +11,30 @@ public class TestaInsercaoComParametro {
 	public static void main(String[] args) throws SQLException {
 				
 		ConnectionFactory factory = new ConnectionFactory();
-		Connection connection = factory.recuperarConexao();
-		connection.setAutoCommit(false);
-		/*
-		 * com esse setAutoCommit(false) tiramos a responsabilidade do JDBC de fazer 
-		 * os Commits das nossas transações, de fato inserir o produto na nossa 
-		 * tabela. Agora eu vou controlar o momento do Commit da minha aplicação, 
-		 * no momento da minha transação.
-		 */
-				
-		try {
-			PreparedStatement stm = connection.prepareStatement(
-				"INSERT INTO PRODUTO (nome, descricao) VALUES (?, ?)",
-				Statement.RETURN_GENERATED_KEYS);
-		
-			adicionarVariavel("SmartTV", "45 polegadas", stm);
-			adicionarVariavel("Radio", "Radio de bateria", stm);
-		
-			connection.commit();
-		
-			stm.close();
-			connection.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-			System.out.println("ROLLBACK EXECUTADO");
-			connection.rollback();
-		}		
+		try (Connection connection = factory.recuperarConexao()) {
+			connection.setAutoCommit(false);
+			/*
+			 * com esse setAutoCommit(false) tiramos a responsabilidade do JDBC de 
+			 * fazer os Commits das nossas transações, de fato inserir o produto na 
+			 * nossa tabela. Agora eu vou controlar o momento do Commit da minha 
+			 * aplicação, no momento da minha transação.
+			 */
+					
+			try (PreparedStatement stm = connection.prepareStatement(
+					"INSERT INTO PRODUTO (nome, descricao) VALUES (?, ?)",
+					Statement.RETURN_GENERATED_KEYS);){
+			
+				adicionarVariavel("SmartTV", "45 polegadas", stm);
+				adicionarVariavel("Radio", "Radio de bateria", stm);
+			
+				connection.commit();
+			
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.out.println("ROLLBACK EXECUTADO");
+				connection.rollback();
+			}
+		}
 	}
 	
 	private static void adicionarVariavel(String nome, String descricao, 
@@ -51,11 +49,11 @@ public class TestaInsercaoComParametro {
 		
 		stm.execute();
 		
-		ResultSet rst = stm.getGeneratedKeys();
-		while (rst.next()) {
-			Integer id = rst.getInt(1);
-			System.out.println("O id criado foi: " + id);
+		try (ResultSet rst = stm.getGeneratedKeys()){
+			while (rst.next()) {
+				Integer id = rst.getInt(1);
+				System.out.println("O id criado foi: " + id);
+			}
 		}
-		rst.close();
 	}
 }
