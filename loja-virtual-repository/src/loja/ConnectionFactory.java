d05a72f7fc8fe69b41ad8d5b267cb7991b266381
sbm-balmeida <beatriz.almeida@sbmtechnology.com.br>
2024-07-18 15:35:43 -0300

    Testando o pool de conexões

--- loja-virtual-repository/src/loja/ConnectionFactory.java
+++ loja-virtual-repository/src/loja/ConnectionFactory.java
@@ -9,15 +9,24 @@ import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 public class ConnectionFactory {
 	
-//	a Datasource que vai fazer com que o nosso Pool funcione
+	/*
+	 * a interface Datasource que vai fazer com que o nosso Pool funcione, vai expor
+	 * as infomações do nosso Pool de conexões.
+	 */
 	public DataSource dataSource;
 	
 	public ConnectionFactory() {
 		ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
+		
 		comboPooledDataSource.setJdbcUrl("jdbc:mysql://localhost/loja_virtual?"
 				+ "useTimezone=true&serverTimezone=UTC");
 		comboPooledDataSource.setUser("root");
 		comboPooledDataSource.setPassword("root");
+		
+		/*o setMaxPoolSize() irá setar o número máximo de conexões disponíveis que 
+		 * eu quero permitir que sejam abertas.
+		 */
+		comboPooledDataSource.setMaxPoolSize(15);
 		
 		this.dataSource = comboPooledDataSource;
 	}
