075416fae008544acba961750fdb4e148a906d91
sbm-balmeida <beatriz.almeida@sbmtechnology.com.br>
2024-07-18 15:14:50 -0300

    Criando o pool de conexões

--- loja-virtual-repository/src/loja/ConnectionFactory.java
+++ loja-virtual-repository/src/loja/ConnectionFactory.java
@@ -1,15 +1,31 @@
 package loja;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 public class ConnectionFactory {
 	
-	public Connection recuperarConexao() throws SQLException {
+//	a Datasource que vai fazer com que o nosso Pool funcione
+	public DataSource dataSource;
+	
+	public ConnectionFactory() {
+		ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
+		comboPooledDataSource.setJdbcUrl("jdbc:mysql://localhost/loja_virtual?"
+				+ "useTimezone=true&serverTimezone=UTC");
+		comboPooledDataSource.setUser("root");
+		comboPooledDataSource.setPassword("root");
 		
-		return DriverManager.getConnection(
-				"jdbc:mysql://localhost/loja_virtual?useTimezone=true&"
-				+ "serverTimezone=UTC",	"root", "root");
+		this.dataSource = comboPooledDataSource;
+	}
+	
+	public Connection recuperarConexao() throws SQLException {
+//		return DriverManager.getConnection(
+//				"jdbc:mysql://localhost/loja_virtual?useTimezone=true&"
+//				+ "serverTimezone=UTC",	"root", "root");
+		return this.dataSource.getConnection();
 	}
 }
