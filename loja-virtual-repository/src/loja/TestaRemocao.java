0b5cca908e26a9994cacd4052da01b88e7be6849
sbm-balmeida <beatriz.almeida@sbmtechnology.com.br>
2024-07-18 10:17:28 -0300

    Removendo dados

---
+++ loja-virtual-repository/src/loja/TestaRemocao.java
@@ -0,0 +1,25 @@
+package loja;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TestaRemocao {
+	
+	public static void main(String[] args) throws SQLException {
+		
+		ConnectionFactory factory = new ConnectionFactory();
+		Connection connection = factory.recuperarConexao();
+		
+		Statement stm = connection.createStatement();
+		stm.execute("DELETE FROM PRODUTO WHERE ID > 2");
+		
+		/*o metodo getUpadateCount() irá retornar um inteiro. Esse inteiro 
+		 * significa, quantas linhas que foram modificadas após o Statement ser 
+		 * executado.*/
+		Integer linhaModificadas = stm.getUpdateCount();
+		System.out.println("Quantidade de linhas que foram modificadas: " + 
+							linhaModificadas);
+	}
+
+}
