d8d0f69d5dcec2d600cc8f5c88fd3fd5a657106b
sbm-balmeida <beatriz.almeida@sbmtechnology.com.br>
2024-07-18 12:20:19 -0300

    Assumindo o controle

--- loja-virtual-repository/src/loja/TestaInsercaoComParametro.java
+++ loja-virtual-repository/src/loja/TestaInsercaoComParametro.java
@@ -9,18 +9,34 @@ import java.sql.Statement;
 public class TestaInsercaoComParametro {
 
 	public static void main(String[] args) throws SQLException {
-		
-		String nome = "Mouse'";
-		String descricao = "Mouse sem fio; delete from produto;";
 				
 		ConnectionFactory factory = new ConnectionFactory();
 		Connection connection = factory.recuperarConexao();
+		/*com esse setAutoCommit(false) tiramos a responsabilidade do JDBC de fazer 
+		 * os Commits das nossas transações, de fato inserir o produto na nossa 
+		 * tabela. Agora eu vou controlar o momento do Commit da minha aplicação, 
+		 * no momento da minha transação.
+		 */
+		connection.setAutoCommit(false);
 		
 		PreparedStatement stm = connection.prepareStatement(
 				"INSERT INTO PRODUTO (nome, descricao) VALUES (?, ?)",
 				Statement.RETURN_GENERATED_KEYS);
+		
+		adicionarVariavel("SmartTV", "45 polegadas", stm);
+		adicionarVariavel("Radio", "Radio de bateria", stm);
+	}
+	
+	private static void adicionarVariavel(String nome, String descricao, 
+			PreparedStatement stm) throws SQLException {
+		
 		stm.setString(1, nome);
 		stm.setString(2, descricao);
+		
+//		if (nome.equals("Radio")) {
+//			throw new RuntimeException("Não foi possível adicionar o produto");
+//		}
+		
 		stm.execute();
 		
 		ResultSet rst = stm.getGeneratedKeys();
@@ -28,5 +44,6 @@ public class TestaInsercaoComParametro {
 			Integer id = rst.getInt(1);
 			System.out.println("O id criado foi: " + id);
 		}
+		rst.close();
 	}
 }
